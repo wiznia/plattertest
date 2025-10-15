ae6c1966e0065c0eb74e74268e05cb98bc039181
Ariel Wiznia <wiznia@gmail.com>
2025-10-15 11:54:27 -0300

    Added smooth transition to elements being shown via show more button

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.show-more').addEventListener('click', function () {
-    const items = document.querySelector('.grid');
+    const itemsContainer = document.querySelector('.hidden-posts');
 
-    items.classList.remove('[&>*:nth-child(n+5)]:hidden');
+    itemsContainer.classList.add('expanded');
     this.remove();
   });
 });
